760b1d4c2935d5ca96c305c7b4d44b2ca25d8960
tatyana86 <Krivonogova_1999@mail.ru>
2023-10-22 20:40:13 +0400

    add solution

---
+++ task1/SimpleTreeTest.java
@@ -0,0 +1,93 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTreeTest {
+
+    SimpleTreeNode<Integer> root = new SimpleTreeNode<>(null, null);
+    SimpleTree<Integer> simpleTree = new SimpleTree<>(root);
+
+    SimpleTreeNode<Integer> fullRoot = new SimpleTreeNode<>(9, null);
+    SimpleTree<Integer> fullSimpleTree = new SimpleTree<>(fullRoot);
+
+    SimpleTreeNode<Integer> node4_firstLevel = new SimpleTreeNode<>(4, fullRoot);
+    SimpleTreeNode<Integer> node17_firstLevel = new SimpleTreeNode<>(17, fullRoot);
+    SimpleTreeNode<Integer> node3_secondLevel = new SimpleTreeNode<>(3, node4_firstLevel);
+    SimpleTreeNode<Integer> node5_secondLevel = new SimpleTreeNode<>(5, node4_firstLevel);
+    SimpleTreeNode<Integer> node7_secondLevel = new SimpleTreeNode<>(7, node4_firstLevel);
+    SimpleTreeNode<Integer> node22_secondLevel = new SimpleTreeNode<>(22, node17_firstLevel);
+
+    @BeforeEach
+    void fillTree() {
+        fullSimpleTree.AddChild(fullRoot, node4_firstLevel);
+        fullSimpleTree.AddChild(fullRoot, node17_firstLevel);
+        fullSimpleTree.AddChild(node4_firstLevel, node3_secondLevel);
+        fullSimpleTree.AddChild(node4_firstLevel, node5_secondLevel);
+        fullSimpleTree.AddChild(node4_firstLevel, node7_secondLevel);
+        fullSimpleTree.AddChild(node17_firstLevel, node22_secondLevel);
+    }
+
+    @Test
+    public void testSimpleTree() {
+        assertEquals(0, simpleTree.Count());
+        assertEquals(0, simpleTree.LeafCount());
+    }
+
+    @Test
+    public void addingNodeInTree() {
+        assertEquals(7, fullSimpleTree.Count());
+        assertEquals(4, fullSimpleTree.LeafCount());
+    }
+
+    @Test
+    public void deleteNodeFromTree() {
+        fullSimpleTree.DeleteNode(node3_secondLevel);
+        assertEquals(6, fullSimpleTree.Count());
+        assertEquals(3, fullSimpleTree.LeafCount());
+
+        fullSimpleTree.DeleteNode(node4_firstLevel);
+        assertEquals(3, fullSimpleTree.Count());
+        assertEquals(1, fullSimpleTree.LeafCount());
+    }
+
+    @Test
+    public void nodesByValue() {
+        SimpleTreeNode<Integer> node5_firstLevel = new SimpleTreeNode<>(5, fullRoot);
+        fullSimpleTree.AddChild(fullRoot, node5_firstLevel);
+
+        List<SimpleTreeNode<Integer>> nodesByValue = fullSimpleTree.FindNodesByValue(5);
+
+        assertTrue(nodesByValue.contains(node5_firstLevel));
+        assertTrue(nodesByValue.contains(node5_secondLevel));
+        assertEquals(2, nodesByValue.size());
+    }
+
+    @Test
+    public void moveNode() {
+        fullSimpleTree.MoveNode(node4_firstLevel, node17_firstLevel);
+        assertEquals(1, fullRoot.Children.size());
+        assertEquals(2, node17_firstLevel.Children.size());
+    }
+
+    @Test
+    public void getNodeLevel() {
+        assertEquals(0, fullSimpleTree.recursionGetNodeLevel(fullRoot));
+        assertEquals(1, fullSimpleTree.recursionGetNodeLevel(node4_firstLevel));
+        assertEquals(1, fullSimpleTree.recursionGetNodeLevel(node17_firstLevel));
+        assertEquals(2, fullSimpleTree.recursionGetNodeLevel(node3_secondLevel));
+        assertEquals(2, fullSimpleTree.recursionGetNodeLevel(node5_secondLevel));
+        assertEquals(2, fullSimpleTree.recursionGetNodeLevel(node7_secondLevel));
+        assertEquals(2, fullSimpleTree.recursionGetNodeLevel(node22_secondLevel));
+    }
+
+    @Test
+    public void printLevels() {
+        fullSimpleTree.printNodesLevel();
+    }
+
+
+}
---
+++ task1/task1.java
@@ -0,0 +1,172 @@
+import java.util.*;
+
+class SimpleTreeNode<T>
+{
+    public T NodeValue;
+    public SimpleTreeNode<T> Parent;
+    public List<SimpleTreeNode<T>> Children;
+
+    public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
+    {
+        NodeValue = val;
+        Parent = parent;
+        Children = null;
+    }
+}
+
+class SimpleTree<T>
+{
+    public SimpleTreeNode<T> Root;
+
+    public SimpleTree(SimpleTreeNode<T> root)
+    {
+        Root = root;
+    }
+
+    public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+    {
+        if(ParentNode.Children == null) {
+            ParentNode.Children = new ArrayList<SimpleTreeNode<T>>();
+        }
+        ParentNode.Children.add(NewChild);
+    }
+
+    public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
+    {
+        NodeToDelete.Parent.Children.remove(NodeToDelete);
+        NodeToDelete.Parent = null;
+        NodeToDelete.Children = null;
+    }
+
+    public List<SimpleTreeNode<T>> GetAllNodes()
+    {
+        if(Root.NodeValue == null) {
+            return new ArrayList<>();
+        }
+        if(Root.Children == null) {
+            List<SimpleTreeNode<T>> allTreeNodes = new ArrayList<SimpleTreeNode<T>>();
+            allTreeNodes.add(Root);
+            return allTreeNodes;
+        }
+
+        return recursionGetAllNodes(Root);
+    }
+
+    private List<SimpleTreeNode<T>> recursionGetAllNodes(SimpleTreeNode<T> nodeForSearchChildren) {
+
+        List<SimpleTreeNode<T>> allTreeNodes = new ArrayList<SimpleTreeNode<T>>();
+        List<SimpleTreeNode<T>> expand = new ArrayList<SimpleTreeNode<T>>();
+
+        allTreeNodes.add(nodeForSearchChildren);
+        expand.addAll(nodeForSearchChildren.Children);
+
+        for(SimpleTreeNode node : expand) {
+            if(node.Children != null) {
+                allTreeNodes.addAll(recursionGetAllNodes(node));
+            }
+            if(node.Children == null) {
+                allTreeNodes.add(node);
+            }
+        }
+
+        return allTreeNodes;
+    }
+
+    public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+    {
+        if(Root.NodeValue == null) {
+            return new ArrayList<>();
+        }
+
+        if(Root.Children == null && Root.NodeValue == val) {
+            List<SimpleTreeNode<T>> nodesByValue = new ArrayList<SimpleTreeNode<T>>();
+            nodesByValue.add(Root);
+            return nodesByValue;
+        }
+
+        if(Root.Children == null && Root.NodeValue != val) {
+            List<SimpleTreeNode<T>> nodesByValue = new ArrayList<SimpleTreeNode<T>>();
+            return nodesByValue;
+        }
+
+        return recursionFindNodesByValue(val, Root);
+    }
+
+    private List<SimpleTreeNode<T>> recursionFindNodesByValue(T val, SimpleTreeNode<T> nodeForCheck) {
+
+        List<SimpleTreeNode<T>> nodesByValue = new ArrayList<SimpleTreeNode<T>>();
+        List<SimpleTreeNode<T>> expand = new ArrayList<SimpleTreeNode<T>>();
+
+        expand.addAll(nodeForCheck.Children);
+
+        for(SimpleTreeNode node : expand) {
+            if(node.Children != null) {
+                nodesByValue.addAll(recursionFindNodesByValue(val, node));
+            }
+            if(node.NodeValue == val && node.Children == null) {
+                nodesByValue.add(node);
+            }
+        }
+
+        return nodesByValue;
+
+    }
+
+    public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+    {
+        OriginalNode.Parent.Children.remove(OriginalNode);
+        AddChild(NewParent, OriginalNode);
+    }
+
+    public int Count()
+    {
+        return GetAllNodes().size();
+    }
+
+    public int LeafCount()
+    {
+        if(Root.NodeValue == null) {
+            return 0;
+        }
+
+        return recursionLeafCount(Root);
+    }
+
+    private int recursionLeafCount(SimpleTreeNode<T> nodeForSearchLeaf) {
+
+        int count = 0;
+
+        if(nodeForSearchLeaf.Children == null || nodeForSearchLeaf.Children.isEmpty()) {
+            count ++;
+            return count;
+        }
+        for(SimpleTreeNode nodeChild : nodeForSearchLeaf.Children) {
+            count += recursionLeafCount(nodeChild);
+        }
+
+        return count;
+    }
+
+    public int recursionGetNodeLevel(SimpleTreeNode<T> node) {
+
+        if(node.Parent == null) {
+            return 0;
+        }
+
+        int level = 1;
+
+        level += recursionGetNodeLevel(node.Parent);
+
+        return level;
+
+    }
+
+    public void printNodesLevel() {
+        List<SimpleTreeNode<T>> allNodes = GetAllNodes();
+
+        for(SimpleTreeNode<T> node : allNodes) {
+            System.out.println("Level of node with value " + node.NodeValue + " - " + recursionGetNodeLevel(node));
+        }
+    }
+
+}
